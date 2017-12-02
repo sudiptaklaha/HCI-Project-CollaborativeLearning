816e8c4fafe0f2fa6e1cf3d70ca620cb5cfecd47
Sudipta <slaha2@uic.edu>
2017-12-02 01:23:12 -0600

    Integration of modules
    Hosted integrated resources in node server.

---
+++ node-server/resource/js/init.js
@@ -0,0 +1,99 @@
+var dropCount = 0;
+var sharedObjCount = 0;
+var rightClickedItem = 0;
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+function drag(ev) {
+  console.log(ev.target.innerHTML);
+  ev.dataTransfer.setData('text/html', ev.target.innerHTML);
+  //$('.ls-row>.col').addClass("z-depth-1");
+}
+function drop(ev) {
+  //console.log('dropped');
+  
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData('text/html');
+  //ev.target.appendChild(data);
+  $(ev.target).append(data);
+  if(++dropCount<=3) {
+    $('#shown-item-list-row').append('<div class="col s4"><div class="row v-list-item">'+data+'</div></div>');
+  }
+  //$('.ls-row>.col').removeClass("z-depth-1");
+  $('.ls-row>.col').removeClass("teal lighten-5 pulse");
+  sharedObjCount++;
+  postToServer(data, ev);
+}
+
+function postToServer(data, ev) {
+  var pickIndex = $(data).attr('index');
+  var dropIndex = $(ev.target).attr('index');
+
+  $.ajax({url: '/drag/?'+pickIndex+','+dropIndex, success: function(result){
+    console.log("posted data", dropIndex);
+  }});
+}
+
+$('document').ready( function () {  
+  $('.ls-row>.col').on('dragover',function() {
+    $( this ).addClass("teal lighten-5 pulse");
+    $('.ls-row>.col').addClass("z-depth-1");
+  });
+  $('.ls-row>.col').on('dragleave',function() {
+    $( this ).removeClass("teal lighten-5 pulse");    
+  });
+  $('.list-item').on('dragend', function() {
+    $('.ls-row>.col').removeClass("z-depth-1");
+  });
+
+  $('.list-item').contextmenu(function(e) {
+    rightClickedItem = $(this).children(":first").attr('index');
+    console.log($(this), e.pageX,e.pageY);
+    $('#dropdown').addClass('active');
+    $('#dropdown').css({
+      'display': 'block',
+      'top':e.pageY-10,
+      'left':e.pageX-10
+    });
+    return false;
+  });
+
+  function closeMenu() {
+    $('#dropdown').removeClass('active');
+    $('#dropdown').css('display','none');
+  };
+
+  $('#dropdown').mouseleave(function() {
+    closeMenu();
+  });
+
+  /**
+   * menu items click
+   * item1:delete item
+   * item2:show in 3D
+   * item3:add to shared screen
+   */
+  $('#menuitem2').click(function() {
+    $.ajax({url: '/change3D?'+rightClickedItem, success: function(result){
+      console.log("changed 3D to", rightClickedItem);
+      }
+    });
+    closeMenu();
+  });
+
+  setInterval(function() {
+    $.ajax({url: '/refresh?'+sharedObjCount, success: function(result){      
+      result.forEach(function(element) {
+        console.log("appending item:", element.item , " :: to position:", element.pos);
+        $.ajax({url: '/resource/html/item'+element.item+'.html', success: function(result){
+          $('#drop'+element.pos).append(result);
+          if(++dropCount<=3) {
+            $('#shown-item-list-row').append('<div class="col s4"><div class="row v-list-item">'+result+'</div></div>');
+          }
+          sharedObjCount++;
+        }});
+      });
+    }});
+  }, 1000);
+});
---
+++ node-server/src/server.js
@@ -0,0 +1,71 @@
+var http = require('http');
+var request = require('request');
+var url = require('url');
+var fs = require('fs');
+var path = require("path");
+
+var shared=[];
+var hologram = 0;
+
+http.createServer(function (req, res) {
+	var q = url.parse(req.url, true);
+	console.log(q.pathname);
+	//console.log(q.search);
+	var pathName = q.pathname;
+	if (pathName.includes('resource')) {
+	  var filename = '..'+pathName;
+
+	  var contentTypesByExtension = {
+		'.html': "text/html",
+		'.css':  "text/css",
+		'.js':   "text/javascript"
+	  };
+
+    //console.log("filename", filename);
+	  fs.exists(filename, function(exists) {
+		if(!exists) {
+			res.writeHead(404, {"Content-Type": "text/plain"});
+			res.write("404 Not Found\n");
+			res.end();
+			return;
+		}
+
+		if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+		fs.readFile(filename, "binary", function(err, file) {
+			if(err) {        
+			res.writeHead(500, {"Content-Type": "text/plain"});
+			res.write(err + "\n");
+			res.end();
+			return;
+			}
+
+			var headers = {};
+			var contentType = contentTypesByExtension[path.extname(filename)];
+			if (contentType) headers["Content-Type"] = contentType;
+			res.writeHead(200, headers);
+			res.write(file, "binary");
+			res.end();
+		});
+	  });
+	} else if (pathName.includes("drag")) {
+		shared.push({"item":q.search.charAt(1), "pos":q.search.charAt(3)});
+		console.log("dragged", q.search, "::shared::", shared);
+		res.writeHead(200, {"Content-Type": "text/plain"});
+		res.end();
+
+	} else if (pathName.includes("refresh")) {
+		res.writeHead(200, {"Content-Type": "application/json"});
+		res.end(JSON.stringify(shared.slice(q.search.charAt(1))));
+
+	} else if (pathName.includes("change3D")) {
+		hologram = q.search.charAt(1)-1;
+		console.log("hologram chnges to:", hologram);
+		res.writeHead(200, {"Content-Type": "text/plain"});
+		res.end();
+		
+	} else if (pathName.includes("get3D")) {
+		res.writeHead(200, {"Content-Type": "text/plain"});
+		res.end(hologram.toString());
+	}
+}).listen(9000);
