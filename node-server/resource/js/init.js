26692fb6959e8db4ae65f7dfddbc08699e11caa8
shreyas111 <shreyaskulkarnilogin@gmail.com>
2017-12-03 23:25:03 -0600

    Final Commit Shreyas

--- node-server/resource/js/init.js
+++ node-server/resource/js/init.js
@@ -2,6 +2,11 @@ var dropCount = 0;
 var sharedObjCount = 0;
 var rightClickedItem = 0;
 
+var detailsObj = -1;
+var rightClickedElem;
+var sharedState = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0};
+
+
 function allowDrop(ev) {
   ev.preventDefault();
 }
@@ -11,25 +16,45 @@ function drag(ev) {
   //$('.ls-row>.col').addClass("z-depth-1");
 }
 function drop(ev) {
+
   //console.log('dropped');
   
   ev.preventDefault();
   var data = ev.dataTransfer.getData('text/html');
   //ev.target.appendChild(data);
   $(ev.target).append(data);
-  if(++dropCount<=3) {
+  
+  $(ev.target).children(":first").addClass('dropped');
+  var index = $(ev.target).children(":first").attr('index');//cursor: pointer;
+  $(ev.target).children(":first").css('cursor','pointer');
+  $(ev.target).children(":first").contextmenu(function(e) {
+    rightClickedElem = this;
+    rightClickedItem = $(this).attr('index');
+    console.log($(this), e.pageX,e.pageY);
+    $('#dropdown2').addClass('active');
+    $('#dropdown2').css({
+      'display': 'block',
+      'top':e.pageY-10,
+      'left':e.pageX-10
+    });
+    return false;
+  });
+  $(ev.target).children(":first").on('click', function() {
+    showDetails(index);
+  });
+
+  /*if(++dropCount<=3) {
     $('#shown-item-list-row').append('<div class="col s4"><div class="row v-list-item">'+data+'</div></div>');
-  }
+  }*/
+
   //$('.ls-row>.col').removeClass("z-depth-1");
   $('.ls-row>.col').removeClass("teal lighten-5 pulse");
   sharedObjCount++;
-  postToServer(data, ev);
+  sharedState[$(ev.target).attr('index')] = 1;
+  postToServer($(data).attr('index'), $(ev.target).attr('index'));
 }
 
-function postToServer(data, ev) {
-  var pickIndex = $(data).attr('index');
-  var dropIndex = $(ev.target).attr('index');
-
+function postToServer(pickIndex, dropIndex) {
   $.ajax({url: '/drag/?'+pickIndex+','+dropIndex, success: function(result){
     console.log("posted data", dropIndex);
   }});
@@ -49,6 +74,7 @@ $('document').ready( function () {
   });
 
   $('.list-item').contextmenu(function(e) {
+    rightClickedElem = this;
     rightClickedItem = $(this).children(":first").attr('index');
     console.log($(this), e.pageX,e.pageY);
     $('#dropdown').addClass('active');
@@ -75,14 +101,65 @@ $('document').ready( function () {
    * item2:show in 3D
    * item3:add to shared screen
    */
+  $('#menuitem1').click(function() {
+    $(rightClickedElem).remove();
+    closeMenu();
+  });
   $('#menuitem2').click(function() {
     $.ajax({url: '/change3D?'+rightClickedItem, success: function(result){
       console.log("changed 3D to", rightClickedItem);
       }
     });
     closeMenu();
+    Materialize.toast('Placed objet to hologram.', 1000, 'rounded');
+  });
+  $('#menuitem3').click(function() {
+    var empty = -1;
+    for (var key in sharedState) {
+        if (sharedState.hasOwnProperty(key) && sharedState[key]==0) {
+          empty = key;
+          break;
+        }
+    }
+    $.ajax({url: '/resource/html/item'+rightClickedItem+'.html', success: function(result){
+      if(empty>=0) {
+        $('#drop'+empty).append(result);
+        sharedObjCount++;
+        sharedState[empty] = 1;
+        postToServer(rightClickedItem, empty);
+      }
+    }});
+    closeMenu();
   });
 
+  /**
+   * shared screen right click menu
+   */
+  function closeMenu2() {
+    $('#dropdown2').removeClass('active');
+    $('#dropdown2').css('display','none');
+  };
+
+  $('#dropdown2').mouseleave(function() {
+    closeMenu2();
+  });
+
+  $('#2menuitem1').click(function() {
+    $(rightClickedElem).remove();
+    closeMenu2();
+  });
+  $('#2menuitem2').click(function() {
+    $('#local-list-container').append(rightClickedElem);
+    //$(rightClickedElem).remove();
+    closeMenu2();
+  });
+
+  /*$('#back').click(function(){
+    console.log('clicked');
+    $.ajax({url: '/resource/html/ls-grid.html', success: function(result){
+      $('#large-screen').html(result);
+    }});
+  });*/
    $('#menuitem4').click(function() {
     closeMenu();
 
@@ -93,9 +170,35 @@ $('document').ready( function () {
   });
 
   setInterval(function() {
+    //console.log(sharedState);
     $.ajax({url: '/refresh?'+sharedObjCount, success: function(result){      
       result.forEach(function(element) {
         console.log("appending item:", element.item , " :: to position:", element.pos);
+
+        if(element.pos==-1) {
+          if(detailsObj!=element.item) {
+            console.log("putting shared object");
+            $.ajax({url: '/resource/html/item'+element.item+'details.html', success: function(result){
+              $('#large-screen').html(result);
+            }});
+            detailsObj=element.item;
+          }
+          sharedObjCount = 0;
+        } else {
+          console.log("putting element");
+          if(detailsObj>=0) {
+            detailsObj = -1;
+            $.ajax({url: '/resource/html/ls-grid.html', success: function(result){
+              $('#large-screen').html(result);
+            }});
+          }
+          $.ajax({url: '/resource/html/item'+element.item+'.html', success: function(result){
+            $('#drop'+element.pos).append(result);
+            sharedObjCount++;
+            sharedState[element.pos] = 1;
+            
+          }});
+        }
         $.ajax({url: '/resource/html/item'+element.item+'.html', success: function(result){
           $('#drop'+element.pos).append(result);
           /*if(++dropCount<=3) {
@@ -103,7 +206,8 @@ $('document').ready( function () {
           }*/
           sharedObjCount++;
         }});
+
       });
     }});
-  }, 1000);
+  }, 2000);
 });
